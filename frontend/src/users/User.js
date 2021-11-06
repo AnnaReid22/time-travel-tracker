cd38242f642944ad074090115001dea411621c7a
Anna Reid <41653027+AnnaReid22@users.noreply.github.com>
2021-11-05 18:26:48 -0700

    oops it was literally doing what it is supposed to and I didn't read it

--- frontend/src/users/User.js
+++ frontend/src/users/User.js
@@ -13,9 +13,6 @@ export default function FormPropsTextFields() {
     email: "",
     password: "",
 });
-const [errors, setErrors] = useState({   
-  email: "",
-})
   function handleChange(event) {
     getUser({
         ...user,
