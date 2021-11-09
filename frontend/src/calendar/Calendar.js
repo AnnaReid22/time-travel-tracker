ab32467b4952f5d6222b88364804c1027d2a953f
Anna Reid <41653027+AnnaReid22@users.noreply.github.com>
2021-11-09 12:02:49 -0800

    calendar updates

--- frontend/src/calendar/Calendar.js
+++ frontend/src/calendar/Calendar.js
@@ -1,16 +1,42 @@
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { useState, useEffect } from "react";
+import { Calendar, momentLocalizer, Views } from "react-big-calendar";
 import moment from "moment";
+import axios from "axios";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const localizer = momentLocalizer(moment);
+let allViews = Object.keys(Views).map(k => Views[k])
 
 export default function MyCalendar() {
+  const [events, setEvents]= useState([]);
+
+  useEffect(() => { 
+    async function getEvents(){
+      try {
+         const response = await axios.get('http://localhost:5000/todos');
+         if(response.status === 201){
+          setEvents(response.data)
+         }
+         else{
+           console.log("Error!!!!")
+         }
+      }
+      catch (error) {
+         console.log(error);
+         return false;
+      }
+     }
+    getEvents();
+  }, []);
+
+
   return (
     <Calendar
+      events={events}
+      step={60}
+      showMultiDayTimes
       localizer={localizer}
-      startAccessor="start"
-      endAccessor="end"
-      defaultView="month"
+      views={allViews}
       style={{ height: "100vh" }}
     />
   );
