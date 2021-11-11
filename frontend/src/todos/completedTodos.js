284b04474fa54baf19d9349dc2a633ac96e4bed8
Jennifer-Vazquez <vazquez.jennifer5@gmail.com>
2021-11-10 19:03:24 -0800

    i swear...

--- frontend/src/todos/completedTodos.js
+++ frontend/src/todos/completedTodos.js
@@ -61,31 +61,6 @@ function descendingComparator(a, b, orderBy) {
 
 
 
-const BootstrapDialogTitle = (props) => {
-    const { children, onClose, ...other } = props;
-
-    return (
-        <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-            {children}
-            {onClose ? (
-                <IconButton
-                    aria-label="close"
-                    onClick={onClose}
-                    sx={{
-                        position: 'absolute',
-                        right: 8,
-                        top: 8,
-                        color: (theme) => theme.palette.grey[500],
-                    }}
-                >
-                    <CloseIcon />
-                </IconButton>
-            ) : null}
-        </DialogTitle>
-    );
-};
-
-
 function getComparator(order, orderBy) {
     return order === 'desc'
         ? (a, b) => descendingComparator(a, b, orderBy)
