94fc0ac24a3f8a37811bac4df7410c4d01b90b05
mayuriprasad <maprasad@calpoly.edu>
2021-11-30 21:14:17 -0800

    100% test coverage is the only thing that keeps me going

--- backend/test_users.py
+++ backend/test_users.py
@@ -80,3 +80,19 @@ def test_find_by_email():
         "cPass":"killme"
     }
     assert(model_mongodb.User().find_by_email(email), user)
+
+def test_update_importance():
+    expected_user = {
+        "_id": "61a701132259d2c284874f52",
+        "firstName": "literally only",
+        "lastName": "exists to pass this one test",
+        "email": "heytheredelilah@newyorkcity.com",
+        "password":"a94a8fe5ccb19ba61c4c0873d391e987982fbbd3",
+        "cPass":"a94a8fe5ccb19ba61c4c0873d391e987982fbbd3",
+        "importanceMeter": [1,2,3,4]
+    }
+    email = "heytheredelilah@newyorkcity.com"
+    importanceMeter = [1,2,3,4]
+    model_mongodb.User().update_importance(email, importanceMeter)
+    new_user = model_mongodb.User().find_by_email(email)
+    assert(new_user, expected_user)
